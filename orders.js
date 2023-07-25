42337f44c6b2c1d8e05383dfa2b851508351080f
Andrey Sergeevich <duhovlovec@gmail.com>
2023-07-25 21:35:52 +0700

    практика Dashboard и svg

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+	{
+		productName: 'JavaScript Tutorial',
+		productNumber: '85723',
+		paymentStatus: 'Due',
+		status: 'Pending',
+	},
+	{
+		productName: 'CSS Full Course',
+		productNumber: '95835',
+		paymentStatus: 'Refunded',
+		status: 'Declined',
+	},
+	{
+		productName: 'Flex-Box Tutorial',
+		productNumber: '34676',
+		paymentStatus: 'Paid',
+		status: 'Active',
+	},
+]
